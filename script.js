57c7055d2c7d9200372250259427b7d98653df17
vinodvulavakayala <vinoddevopscloud99@gmail.com>
2024-05-19 00:24:26 -0500

    added terminal text and tux

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const text = "Hello, I'm Vinod, a seasoned DevOps and Cloud Engineer with a passion for optimizing and automating the software development lifecycle. With extensive expertise in Linux, cloud computing, and a range of DevOps tools, I specialize in creating efficient, scalable, and secure infrastructure solutions. My experience spans across deploying and managing applications in various cloud environments, leveraging technologies like Docker, Kubernetes, Terraform, and Azure DevOps to streamline workflows and enhance productivity. Whether it's setting up Kubernetes clusters, implementing CI/CD pipelines, or ensuring the seamless";
+    const commandOutput = document.getElementById('command-output');
+    const cursor = document.querySelector('.cursor');
+    let index = 0;
+
+    function type() {
+        if (index < text.length) {
+            commandOutput.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, 50); // Adjust delay between each character
+        } else {
+            clearInterval(cursorBlinking); // Stop cursor blinking when typing is done
+            cursor.style.display = 'none'; // Optionally hide cursor after typing is done
+        }
+    }
+
+    type();
+
+    // Keep the cursor blinking indefinitely
+    const cursorBlinking = setInterval(() => {
+        if (cursor.style.display === 'none') {
+            cursor.style.display = 'inline-block';
+        } else {
+            cursor.style.display = 'none';
+        }
+    }, 500); // Blinking interval for the cursor
+});
